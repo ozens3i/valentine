422e2b33a0e7ae50132ceef74acb6caa093a09a7
ozens3i <160055776+ozens3i@users.noreply.github.com>
2024-02-15 01:58:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+
+
+  var clickCount = 0;
+  var texts = [
+      "Happy Valentine's Day!",
+      "Roses are red, violets are blue,",
+      "I'm grateful to have a friend like you 🌹",
+      "- ChatGPT",
+      "JK Sorry nagcricringe ako isulat to 😫",
+      "Pero maraming salamat!",
+      "And I hope you are doing good! 👍",
+      "Your presence means so much to me 💖",
+      "I know I'm late but 😣",
+      "I hope this flower brightens up your day 🌻",
+      "Thanks for being the amazing friend that you are 😎",
+      "'Di ko na alam ano pa isusulat pero...",
+      "Wishing you all the best on exams tomorrow 🙏",
+      "I believe you can ace it 💯",
+      "Again...",
+      "Happy Valentine's Day 🤞",
+  ];
+  
+  document.addEventListener('click', function() {
+      changeText();
+  });
+  
+  function changeText() {
+      var valentineText = document.getElementById("valentineText");
+      
+      // Check if clickCount is within the bounds of the texts array
+      if (clickCount < texts.length) {
+          valentineText.innerHTML = texts[clickCount];
+          clickCount++;
+  
+          // If clickCount reaches the end, remove the click event listener
+          if (clickCount === texts.length) {
+              document.removeEventListener('click', changeText);
+          }
+      }
+  }
